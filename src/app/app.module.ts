18013e76f0a9e8d3b940ae4969217a84f288783d
Sriram <king.sriram.net@gmail.com>
2019-12-04 13:53:25 -0500

    added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,32 @@ import { AppRoutingModule } from './app-routing.module';
 
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { LoadingBarComponent } from './loading-bar/loading-bar.component';
+import { QuickviewComponent } from './quickview/quickview.component';
+import { HeaderComponent } from './header/header.component';
+import { HeaderTopbarComponent } from './header-topbar/header-topbar.component';
+import { PageHomeComponent } from './page-home/page-home.component';
+import { BlockSlideshowComponent } from './block-slideshow/block-slideshow.component';
+import { FooterComponent } from './footer/footer.component';
+import { FooterContactsComponent } from './footer-contacts/footer-contacts.component';
+import { FooterLinksComponent } from './footer-links/footer-links.component';
+import { FooterNewsletterComponent } from './footer-newsletter/footer-newsletter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LoadingBarComponent,
+    QuickviewComponent,
+    HeaderComponent,
+    HeaderTopbarComponent,
+    PageHomeComponent,
+    BlockSlideshowComponent,
+    FooterComponent,
+    FooterContactsComponent,
+    FooterLinksComponent,
+    FooterNewsletterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/block-slideshow/block-slideshow.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-block-slideshow',
+  templateUrl: './block-slideshow.component.html',
+  styleUrls: ['./block-slideshow.component.scss']
+})
+export class BlockSlideshowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
